820a183c179c39e33a2d3299708ba8955bc6d1e3
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-15 17:45:56 -0700

    finished repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, numOfRepetitions) {
+  if (str === '') {
+    return str;
+  } else if (numOfRepetitions < 0) {
+    return 'ERROR';
+  }
 
+  let repeatedStr = '';
+  while (numOfRepetitions > 0) {
+    repeatedStr += str;
+    numOfRepetitions--;
+  }
+  return repeatedStr;
 }
 
 module.exports = repeatString
