6bcce5d82c435857d35dd8ad8b35ac02e143a5b9
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-15 21:10:48 -0700

    completed leapYears.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let isLeapYear = year % 4 === 0;
+  if (year % 100 === 0) {
+    return isLeapYear && (year % 400 === 0);
+  } else {
+    return isLeapYear;
+  }
 }
 
 module.exports = leapYears
