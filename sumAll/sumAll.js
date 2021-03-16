b00ae9e5a23e88978f46fbe562b9774fcc591f0e
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-15 18:41:21 -0700

    completed sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  let smallerNumber = Math.min(firstNumber, secondNumber);
 
+  if (isNaN(smallerNumber) || smallerNumber < 0) {
+    return 'ERROR';
+  }
+
+  let largerNumber = Math.max(firstNumber, secondNumber);
+  /* Taking average of smaller and larger numbers and multiplying by the amount of numbers */
+  return ((smallerNumber +largerNumber) / 2) * (largerNumber - smallerNumber + 1);
 }
 
 module.exports = sumAll
