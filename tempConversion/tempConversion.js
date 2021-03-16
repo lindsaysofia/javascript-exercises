a695f972de35a9d3965901d854508ccc93151880
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-15 21:22:19 -0700

    completed tempConversion exercise. Needed to convert result to number

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitValue) {
+  let celsiusValue = (fahrenheitValue - 32) * (5/9);
+  return +celsiusValue.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(celsiusValue) {
+  let fahrenheitValue = celsiusValue * (9/5) + 32;
+  return +fahrenheitValue.toFixed(1);
 }
 
 module.exports = {
