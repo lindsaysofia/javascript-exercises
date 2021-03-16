8a093c8b320f8a50b2527f87b5e6560ab2e1380e
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-15 18:21:25 -0700

    completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let arrrWithRemovals = [];
+  arr.forEach(element => {
+    if (!args.includes(element)) {
+      arrrWithRemovals.push(element);
+    }
+  });
+  return arrrWithRemovals;
 }
 
 module.exports = removeFromArray
